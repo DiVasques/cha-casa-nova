541612e1b4764393bd6c3a49ee0305712a56cb3f
DiVasques <49960268+DiVasques@users.noreply.github.com>
2023-01-27 01:48:26 -0300

    feat: added border radius on base scaffold and inverted background colors (#5)

--- lib/ui/widgets/base_scaffold.dart
+++ lib/ui/widgets/base_scaffold.dart
@@ -7,31 +7,43 @@ class BaseScaffold extends Scaffold {
     super.appBar,
     super.extendBodyBehindAppBar = true,
   }) : super(
-            body: Container(
-          constraints: const BoxConstraints.expand(),
-          child: Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: Image.asset(
-                  'images/base_grid.png',
-                  repeat: ImageRepeat.repeat,
-                ),
-              ),
-              SingleChildScrollView(
-                padding: const EdgeInsets.only(
-                    top: 100, left: 30, right: 30, bottom: 50),
-                child: Center(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 15),
-                    width: 750,
-                    height: 1500,
-                    color: Colors.white,
-                    child: body,
+          body: Container(
+            constraints: const BoxConstraints.expand(),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Image.asset(
+                    'images/base_grid_invert.png',
+                    repeat: ImageRepeat.repeat,
                   ),
                 ),
-              ),
-            ],
+                SingleChildScrollView(
+                  padding: const EdgeInsets.only(
+                    top: 100,
+                    left: 30,
+                    right: 30,
+                    bottom: 50,
+                  ),
+                  child: Center(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 15,
+                      ),
+                      width: 750,
+                      height: 1500,
+                      child: body,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ));
+        );
 }
