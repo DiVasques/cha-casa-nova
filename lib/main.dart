f363c387e0650e1e47999e4b7cc9285d483be335
Diogo Vasques <divasques12.11@gmail.com>
2023-01-28 04:34:26 -0300

    feat: added firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:cha_casa_nova/ui/routers/generic_router.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cha_casa_nova/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChaDeCasaNova());
 }
 
