baeed49b39dc97017c6662e8fb1db840f259a0c5
Diogo Vasques <divasques12.11@gmail.com>
2023-01-28 02:19:49 -0300

    feat: created basic login page and added montserrat font

--- lib/ui/utils/app_colors.dart
+++ lib/ui/utils/app_colors.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cha_casa_nova/ui/utils/hex_color.dart';
 
 class AppColors {
-  static final HexColor appBarBlue = HexColor('3bc2d7');
+  static final HexColor appDarkGreen = HexColor('2d7565');
+  static final HexColor appLightGreen = HexColor('59c0a9');
 
   static const Color appBarIconColor = Colors.white;
 }
