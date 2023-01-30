bb218999dbccdf917f4fa9c9858ef3ad85ab2061
Diogo Vasques <divasques12.11@gmail.com>
2023-01-30 15:49:40 -0300

    feat: user is now redirected to login if not logged in

--- lib/ui/routers/generic_router.dart
+++ lib/ui/routers/generic_router.dart
@@ -11,6 +11,14 @@ class GenericRouter {
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     WidgetBuilder builder;
+
+    if (settings.arguments == null) {
+      RouteSettings redirectSettings = const RouteSettings(name: loginRoute);
+      builder = (BuildContext _) => LoginPage();
+      return MaterialPageRoute<dynamic>(
+          builder: builder, settings: redirectSettings);
+    }
+
     switch (settings.name) {
       case loginRoute:
         builder = (BuildContext _) => LoginPage();
@@ -28,7 +36,7 @@ class GenericRouter {
           builder: (_) {
             return Scaffold(
               body: Center(
-                child: Text('Rota não definida para ${settings.name}'),
+                child: Text('404: Rota não definida para ${settings.name}'),
               ),
             );
           },
